4f5c5b0edc88cb1770c8679f67092e6d6f50e372
Peter Melinat <petermelint@MacBook-Pro-von-Peter.local>
2022-06-16 16:18:06 +0200

    added lambda

---
+++ lambda/lib/lambda-stack.ts
@@ -0,0 +1,14 @@
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+
+export class LambdaStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    new lambda.Function(this, "LambdaHandler", {
+      code: lambda.Code.fromAsset("../artifacts"),
+      handler: "unrelated",
+      runtime: lambda.Runtime.PROVIDED_AL2,
+    });
+    // The code that defines your stack goes here
+  }
+}
