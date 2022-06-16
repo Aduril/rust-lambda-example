ff2b4885c0297cce1ed34aca53c6f301e4b24978
Peter Melinat <petermelint@MacBook-Pro-von-Peter.local>
2022-06-16 17:03:35 +0200

    why looking at the payload in an example?

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,9 @@ use lambda_http::{IntoResponse, Request};
 use lambda_runtime::{self, Error};
 use jemallocator;
 
-async fn apply(mut req: Request, _c: Context) -> Result<impl IntoResponse, Error> {
-    let payload = req.body_mut();
-    match payload {
-        _ => {
-            println!("hello!");
-            Ok("Hello World")
-        },
-    }
+async fn apply(mut _req: Request, _c: Context) -> Result<impl IntoResponse, Error> {
+    println!("hello!");
+    Ok("Hello World")
 }
 
 #[global_allocator]
